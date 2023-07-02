92bbce1309d7feeb59ef091dfcc542ac5fa109ac
shige0309 <qqa27yed@chic.ocn.ne.jp>
2023-07-02 23:35:59 +0900

    vercelデプロイ準備

--- api/index.js
+++ api/index.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT || 4000;
 
 //データベース接続
 mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log("DBと接続中...");
-}).catch((err) => {
-    console.log(err);
-}) 
+// .then(() => {
+//     console.log("DBと接続中...");
+// }).catch((err) => {
+//     console.log(err);
+// })
 
 //ミドルウェア
 app.use("/images", express.static(path.join(__dirname, "public/images")));
@@ -35,4 +35,4 @@ app.use("/api/upload", uploadRoute);
 app.use("/api/email", emailRoute);
 app.use("/api/imageUpload", imageUploadRoute);
 
-app.listen(PORT, () => console.log("サーバーが起動しました。"));
+// app.listen(PORT, () => console.log("サーバーが起動しました。"));
