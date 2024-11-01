e4cf00890a8b087b12a402e8e9c5fced7a1d33a5
shige0309 <qqa27yed@chic.ocn.ne.jp>
2024-11-01 18:01:57 +0900

    verifyToken修正

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   try {
     const bearToken = req.headers["authorization"];
+    if (!bearToken) {
+      return res.status(400).json("Authorizationヘッダーがありません。");
+    }
     const token = bearToken.split(" ")[1];
 
     if (!token) {
@@ -11,14 +14,14 @@ const verifyToken = (req, res, next) => {
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decode) => {
       if (err) {
-        return res.status(400).json(err);
+        return res.status(400).json("トークンの検証に失敗しました。");
       } else {
         req.id = decode.id;
         next();
       }
     });
   } catch (error) {
-    return res.status(401).json(error.message);
+    return res.status(401).json("サーバーエラーが発生しました。");
   }
 };
 
